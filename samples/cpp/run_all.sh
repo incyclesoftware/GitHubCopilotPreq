f666b68d9a250ed257f0cead3402c2167100f1c9
Tal Riginiano <tal@talreg.com>
2025-03-26 19:48:15 -0400

    Add pause between sample runs in run_all.sh for better user experience

--- samples/cpp/run_all.sh
+++ samples/cpp/run_all.sh
@@ -2,4 +2,7 @@ options=("cmake" "gtest" "boost")
 for dir in "${options[@]}"; do
     cd $dir && make run
     cd ..
+    echo "============================="
+    echo "Hit enter to continue"
+    read
 done
