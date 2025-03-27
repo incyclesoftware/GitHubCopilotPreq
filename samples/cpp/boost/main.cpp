dee18fdd6f43d6b031ea84952463ce13fd943660
Tal Riginiano <tal@talreg.com>
2025-03-26 20:15:42 -0400

    Refactor echo server to use class-based sessions for improved readability and maintainability

--- samples/cpp/boost/main.cpp
+++ samples/cpp/boost/main.cpp
@@ -3,61 +3,95 @@
 #include <string>
 #include <atomic>
 #include <thread>
+#include <memory>
 
 using namespace boost::asio;
 using ip::tcp;
 
-void session(tcp::socket sock) {
-    try {
-        char data[1024];
-        while (true) {
-            boost::system::error_code error;
-            size_t length = sock.read_some(buffer(data), error);
-            if (error == boost::asio::error::eof) {
-                break;
-            } else if (error) {
-                throw boost::system::system_error(error);
-            }
-            write(sock, buffer(data, length));
-        }
-    } catch (std::exception& e) {
-        std::cerr << "Exception in thread: " << e.what() << "\n";
-    }
-}
+class Session : public std::enable_shared_from_this<Session> {
+public:
+    explicit Session(tcp::socket socket)
+        : socket_(std::move(socket)) {}
 
-void run_server(io_context& io, std::atomic<bool>& running, tcp::acceptor& acceptor) {
-    try {
-        std::cout << "Echo server running on port 25252...\n";
-
-        while (running) {
-            tcp::socket socket(io);
-            boost::system::error_code ec;
-
-            // Use async_accept with a timeout to avoid blocking indefinitely
-            acceptor.async_accept(socket, [&](const boost::system::error_code& error) {
-                if (!error && running) {
-                    std::cout << "Connection accepted.\n";
-                    std::thread(session, std::move(socket)).detach();
-                } else if (error && error != boost::asio::error::operation_aborted) {
-                    std::cerr << "Error in accept: " << error.message() << "\n";
+    void start() {
+        this->socket_.async_send(buffer("Welcome to Echo server.\n"),
+            [this](boost::system::error_code ec, std::size_t /*length*/) {
+                if (!ec) {
+                    do_read();
+                } else {
+                    std::cerr << "Send error: " << ec.message() << "\n";
                 }
             });
-
-            // Run the io_context for a short duration to process async_accept
-            io.run_one();
-        }
-    } catch (std::exception& e) {
-        std::cerr << "Exception in server thread: " << e.what() << "\n";
+        do_read();
     }
-}
+
+private:
+    void do_read() {
+        auto self(shared_from_this());
+        socket_.async_read_some(buffer(data_),
+            [this, self](boost::system::error_code ec, std::size_t length) {
+                if (!ec) {
+                    do_write(length);
+                } else if (ec != boost::asio::error::eof) {
+                    std::cerr << "Read error: " << ec.message() << "\n";
+                }
+            });
+    }
+
+    void do_write(std::size_t length) {
+        auto self(shared_from_this());
+        async_write(socket_, buffer(data_, length),
+            [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                if (!ec) {
+                    do_read();
+                } else {
+                    std::cerr << "Write error: " << ec.message() << "\n";
+                }
+            });
+    }
+
+    tcp::socket socket_;
+    char data_[1024];
+};
+
+class Server {
+public:
+    Server(io_context& io, std::atomic<bool>& running, short port)
+        : acceptor_(io, tcp::endpoint(tcp::v4(), port)), running_(running) {
+        do_accept();
+    }
+
+private:
+    void do_accept() {
+        acceptor_.async_accept(
+            [this](boost::system::error_code ec, tcp::socket socket) {
+                if (!ec && running_) {
+                    std::cout << "Connection accepted.\n";
+                    std::make_shared<Session>(std::move(socket))->start();
+                } else if (ec && ec != boost::asio::error::operation_aborted) {
+                    std::cerr << "Accept error: " << ec.message() << "\n";
+                }
+
+                if (running_) {
+                    do_accept();
+                }
+            });
+    }
+
+    tcp::acceptor acceptor_;
+    std::atomic<bool>& running_;
+};
 
 int main() {
     io_context io;
     std::atomic<bool> running{true};
-    tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), 25252));
-    std::thread server_thread(run_server, std::ref(io), std::ref(running), std::ref(acceptor));
+    Server server(io, running, 25252);
+
+    std::thread io_thread([&io]() { io.run(); });
 
     std::string command;
+    std::cout << "Server started.\n";
+    std::cout << "Type 'quit' to shut down server.\n";
     while (running) {
         std::getline(std::cin, command);
         command.erase(0, command.find_first_not_of(" \t\n\r"));
@@ -66,20 +100,14 @@ int main() {
         if (command == "quit") {
             std::cout << "Shutting down server...\n";
             running = false;
-            acceptor.cancel();
-            acceptor.close();
             io.stop();
-            try {
-                acceptor.cancel();
-            } catch (...) {
-            }
         } else {
             std::cout << "unknown command\n";
         }
     }
 
-    std::cout << "Server shut down.\nwaiting for server thread to finish...\n";
-    server_thread.join();
+    io_thread.join();
+    std::cout << "Server shut down.\n";
 
     return 0;
 }
